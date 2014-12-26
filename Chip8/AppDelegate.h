32270a867382cd3a9fd4296f55042bbb59094bd1
Lee Morgan <leemorgan@me.com>
2014-12-25 20:28:56 -0500

    Initial Commit

---
+++ Chip8/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chip8
+//
+//  Created by Lee Morgan on 12/25/14.
+//  Copyright (c) 2014 Lee Morgan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Chip8/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Chip8
+//
+//  Created by Lee Morgan on 12/25/14.
+//  Copyright (c) 2014 Lee Morgan. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
